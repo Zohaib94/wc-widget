f745dd855cc80d8877e37c90102fb20998966ecd
Zohaib Ahmed <zohaibahmedbutt@gmail.com>
2018-06-15 21:21:10 +0500

    Converted from chrome extension to webpage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <Provider store={store}>
         <div className="App">
-          <header className="App-header">
+          <header className="App-header container">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Today's Matches</h1>
           </header>
